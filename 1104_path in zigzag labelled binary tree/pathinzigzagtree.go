7714d36d8643173f5dd572d0f94799a960ecfae8
mrandrmrsbenben <7120230@qq.com>
2019-07-03 15:51:09 +0800

    Problem 1104 second commit

--- 1104_path in zigzag labelled binary tree/pathinzigzagtree.go
+++ 1104_path in zigzag labelled binary tree/pathinzigzagtree.go
@@ -3,14 +3,49 @@ package main
 import "fmt"
 
 func main() {
-	// label := 14
-	label := 1000000
+	label := 14
+	// label := 26
 	fmt.Println("Output:", pathInZigZagTree(label))
+}
+
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Path In Zigzag Labelled Binary Tree.
+// Memory Usage: 1.9 MB, less than 100.00% of Go online submissions for Path In Zigzag Labelled Binary Tree.
+func pathInZigZagTree(label int) []int {
+	if label == 1 {
+		return []int{1}
+	}
+	n := 2
+	level := 1
+	for n*2 <= label {
+		level++
+		n *= 2
+	}
+	level++
+	var index int
+	if level%2 == 0 {
+		index = n*2 - 1 - label
+	} else {
+		index = label - n
+	}
+	path := make([]int, level)
+	path[0] = 1
+	path[level-1] = label
+	for i := level - 2; i > 0; i-- {
+		level--
+		n /= 2
+		index /= 2
+		if level%2 == 0 {
+			path[i] = n*2 - 1 - index
+		} else {
+			path[i] = n + index
+		}
+	}
+	return path
 }
 
 // Runtime: 12 ms, faster than 8.33% of Go online submissions for Path In Zigzag Labelled Binary Tree.
 // Memory Usage: 24.3 MB, less than 100.00% of Go online submissions for Path In Zigzag Labelled Binary Tree.
-func pathInZigZagTree(label int) []int {
+func pathInZigZagTreeV1(label int) []int {
 	tree := make([][]int, 1)
 	leaves := []int{1}
 	tree[0] = leaves
