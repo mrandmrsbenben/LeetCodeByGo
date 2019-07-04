9ab315328d379ef8fb65393f842f80438dd588a5
mrandrmrsbenben <7120230@qq.com>
2019-07-04 19:30:55 +0800

    P89 first commit

---
+++ 89_gray code/graycode.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Output:", grayCode(3))
+}
+
+//执行用时 :4 ms, 在所有 Go 提交中击败了94.59%的用户
+//内存消耗 :7.7 MB, 在所有 Go 提交中击败了14.64%的用户
+func grayCode(n int) []int {
+	ns := []int{0, 1}
+	switch n {
+	case 0:
+		return []int{0}
+	case 1:
+		return ns
+	}
+	bit := 2
+	for n > 1 {
+		as := make([]int, len(ns))
+		j := len(as) - 1
+		for i := range ns {
+			as[j] = ns[i] + bit
+			j--
+		}
+		ns = append(ns, as...)
+		n--
+		bit *= 2
+	}
+	for _, n := range ns {
+		fmt.Printf("%03b\n", n)
+	}
+	return ns
+}
