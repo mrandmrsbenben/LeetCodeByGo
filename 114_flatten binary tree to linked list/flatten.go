aa3dd7af0e6b8723af75fe7d4b70e81bf8e7098f
mrandrmrsbenben <7120230@qq.com>
2019-07-10 19:40:50 +0800

    P114 first update add two solutions

--- 114_flatten binary tree to linked list/flatten.go
+++ 114_flatten binary tree to linked list/flatten.go
@@ -15,9 +15,46 @@ func main() {
 	fmt.Println("Output:", root)
 }
 
+//执行用时 :4 ms, 在所有 Go 提交中击败了100.00%的用户
+//内存消耗 :7.1 MB, 在所有 Go 提交中击败了17.86%的用户
+func flatten(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	var node, buf *TreeNode
+	node = root
+	for node != nil {
+		if node.Left != nil {
+			buf = node.Left
+			for buf.Right != nil {
+				buf = buf.Right
+			}
+			buf.Right, node.Right = node.Right, node.Left
+			node.Left = nil
+		}
+		node = node.Right
+	}
+}
+
+//执行用时 :0 ms, 在所有 Go 提交中击败了100.00%的用户
+//内存消耗 :6.8 MB, 在所有 Go 提交中击败了46.43%的用户
+func flattenV2(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	flatten(root.Left)
+	flatten(root.Right)
+	r := root.Right
+	root.Right, root.Left = root.Left, nil
+	for root.Right != nil {
+		root = root.Right
+	}
+	root.Right = r
+}
+
 //执行用时 :8 ms, 在所有 Go 提交中击败了91.78%的用户
 //内存消耗 :6.6 MB, 在所有 Go 提交中击败了71.43%的用户
-func flatten(root *TreeNode) {
+func flattenV1(root *TreeNode) {
 	if root == nil {
 		return
 	}
