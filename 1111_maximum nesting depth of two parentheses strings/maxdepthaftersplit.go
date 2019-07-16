6ec3ecefa1d1d5ddd104a34265e7d85781af0b21
mrandrmrsbenben <7120230@qq.com>
2019-07-16 14:53:47 +0800

    P1111 update

--- 1111_maximum nesting depth of two parentheses strings/maxdepthaftersplit.go
+++ 1111_maximum nesting depth of two parentheses strings/maxdepthaftersplit.go
@@ -8,6 +8,8 @@ func main() {
 	fmt.Println("Output:", maxDepthAfterSplit(seq))
 }
 
+//执行用时 :60 ms, 在所有 Go 提交中击败了100.00%的用户
+//内存消耗 :7.9 MB, 在所有 Go 提交中击败了100.00%的用户
 func maxDepthAfterSplit(seq string) []int {
 	answer := make([]int, len(seq))
 	a, b := 0, 0
