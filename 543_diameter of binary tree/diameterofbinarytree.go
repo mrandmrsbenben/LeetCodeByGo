4a54af1887427b6712f8ec1a28135961b3537a83
mrandrmrsbenben <7120230@qq.com>
2020-03-10 09:47:22 +0800

    P543 Update

--- 543_diameter of binary tree/diameterofbinarytree.go
+++ 543_diameter of binary tree/diameterofbinarytree.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+// TreeNode Tree Node
 type TreeNode = common.TreeNode
 
 func main() {
@@ -13,27 +14,59 @@ func main() {
 }
 
 func diameterOfBinaryTree(root *TreeNode) int {
-	if root == nil {
-		return 0
-	}
-	lmax := maxDepth(root.Left)
-	rmax := maxDepth(root.Right)
-	max := lmax + rmax
-	var ld, rd int
-	if lmax*2 > max {
-		ld = diameterOfBinaryTree(root.Left)
-	}
-	if rmax*2 > max {
-		rd = diameterOfBinaryTree(root.Right)
-	}
-	if ld > max || rd > max {
-		if ld > rd {
-			return ld
+	max := 0
+	var diameter func(r *TreeNode)
+	diameter = func(r *TreeNode) {
+		if r == nil {
+			return
 		}
-		return rd
+		lmax := maxDepth(r.Left)
+		rmax := maxDepth(r.Right)
+		if lmax+rmax > max {
+			max = lmax + rmax
+		} else if (lmax-1)*2 <= max && (rmax-1)*2 <= max {
+			return
+		}
+		var ld, rd int
+		if lmax > rmax && (lmax-1)*2 > max {
+			ld = diameterOfBinaryTree(root.Left)
+			if ld > max {
+				max = ld
+			}
+		}
+		if rmax > lmax && (rmax-1)*2 > max {
+			rd = diameterOfBinaryTree(root.Right)
+			if rd > max {
+				max = rd
+			}
+		}
 	}
+	diameter(root)
 	return max
 }
+
+// func diameterOfBinaryTree(root *TreeNode) int {
+// 	if root == nil {
+// 		return 0
+// 	}
+// 	lmax := maxDepth(root.Left)
+// 	rmax := maxDepth(root.Right)
+// 	max := lmax + rmax
+// 	var ld, rd int
+// 	if lmax*2 > max {
+// 		ld = diameterOfBinaryTree(root.Left)
+// 	}
+// 	if rmax*2 > max {
+// 		rd = diameterOfBinaryTree(root.Right)
+// 	}
+// 	if ld > max || rd > max {
+// 		if ld > rd {
+// 			return ld
+// 		}
+// 		return rd
+// 	}
+// 	return max
+// }
 
 func maxDepth(t *TreeNode) int {
 	if t == nil {
