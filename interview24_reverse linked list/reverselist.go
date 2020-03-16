b8ee91d1594e74c025c74b6f14108e292ad21669
mrandrmrsbenben <7120230@qq.com>
2020-03-16 15:42:56 +0800

    INT24 Add

---
+++ interview24_reverse linked list/reverselist.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	common "LeetCodeByGo/0_common"
+	"fmt"
+)
+
+type ListNode = common.ListNode
+
+// https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+func main() {
+	input := "1->2->3->4->5->NULL"
+	fmt.Printf("Output: %v\n", reverseList(common.MakeListNode(input)))
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	node := reverseList(head.Next)
+	head.Next.Next, head.Next = head, nil
+	return node
+}
+
+func reverseListV1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var prev, cur, next *ListNode
+	cur = head
+	for cur != nil {
+		next = cur.Next
+		cur.Next, prev = prev, cur
+		cur = next
+	}
+	return prev
+}
