014de0a98a18812971565a9867a8fcee006b083a
mrandrmrsbenben <7120230@qq.com>
2020-03-27 10:16:04 +0800

    P914 Update

--- 914_x of a kind in a deck of cards/hasgroupsizex.go
+++ 914_x of a kind in a deck of cards/hasgroupsizex.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 )
 
+// https://leetcode-cn.com/problems/x-of-a-kind-in-a-deck-of-cards/
 func main() {
 	// common.MakeTestCases("hasGroupsSizeX")
 	input1 := []int{1, 2, 3, 4, 4, 3, 2, 1}
@@ -22,6 +23,12 @@ func main() {
 	input5 := []int{1, 1, 2, 2, 2, 2}
 	fmt.Printf("Expect5: true\n")
 	fmt.Printf("Output5: %v\n", hasGroupsSizeX(input5))
+	input6 := []int{1, 1, 1, 1, 2, 2, 2, 2, 2, 2}
+	fmt.Printf("Expect6: true\n")
+	fmt.Printf("Output6: %v\n", hasGroupsSizeX(input6))
+	input7 := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3}
+	fmt.Printf("Expect7: true\n")
+	fmt.Printf("Output7: %v\n", hasGroupsSizeX(input7))
 }
 
 //执行用时 :24 ms, 在所有 Go 提交中击败了93.75%的用户
@@ -37,6 +44,8 @@ func hasGroupsSizeX(deck []int) bool {
 	var found bool
 	for i := 2; i <= len(deck)/2; i++ {
 		if len(deck)%i != 0 {
+			continue
+		} else if i > 2 && i%2 == 0 {
 			continue
 		}
 		found = true
